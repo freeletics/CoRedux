1acfae4194c4316060432e4817fcb82ff44759a9
Yahor Berdnikau <yahor@freeletics.com>
2018-10-20 10:01:18 +0200

    First iteration on coroutine store.

--- library/src/main/kotlin/com/freeletics/rxredux/ObservableReduxStore.kt
+++ library/src/main/kotlin/com/freeletics/rxredux/ObservableReduxStore.kt
@@ -1,17 +1,12 @@
 package com.freeletics.rxredux
 
-import io.reactivex.Observable
-import io.reactivex.Observer
-import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.disposables.Disposable
-import io.reactivex.observers.SerializedObserver
-import io.reactivex.plugins.RxJavaPlugins
-import io.reactivex.subjects.PublishSubject
-import io.reactivex.subjects.Subject
-import kotlinx.coroutines.experimental.CoroutineScope
 import kotlinx.coroutines.experimental.Dispatchers
+import kotlinx.coroutines.experimental.GlobalScope
+import kotlinx.coroutines.experimental.channels.Channel
+import kotlinx.coroutines.experimental.channels.ReceiveChannel
+import kotlinx.coroutines.experimental.channels.toChannel
 import kotlinx.coroutines.experimental.launch
-import kotlinx.coroutines.experimental.rx2.openSubscription
+import kotlinx.coroutines.experimental.runBlocking
 import kotlin.coroutines.experimental.CoroutineContext
 
 /**
@@ -33,19 +28,39 @@ import kotlin.coroutines.experimental.CoroutineContext
  * @param S The type of the State
  * @param A The type of the Actions
  */
-fun <S: Any, A: Any> Observable<A>.reduxStore(
+fun <S: Any, A: Any> ReceiveChannel<A>.reduxStore(
     initialState: S,
     sideEffects: List<SideEffect<S, A>>,
+    coroutineContext: CoroutineContext = Dispatchers.Unconfined,
     reducer: Reducer<S, A>
-): Observable<S> {
-    return RxJavaPlugins.onAssembly(
-        ObservableReduxStore(
-            initialState = initialState,
-            upstreamActionsObservable = this,
-            reducer = reducer,
-            sideEffects = sideEffects
-        )
-    )
+): ReceiveChannel<S> {
+    val output = Channel<S>()
+    val upstreamChannel = this
+    val actionsChannel = Channel<A>()
+    var currentState = initialState
+
+    runBlocking {
+        output.send(currentState)
+    }
+
+    GlobalScope.launch(coroutineContext) {
+        upstreamChannel.toChannel(actionsChannel)
+    }
+
+    sideEffects.forEach { sideEffect ->
+        GlobalScope.launch(coroutineContext) {
+            sideEffect(actionsChannel) { return@sideEffect currentState }.toChannel(actionsChannel)
+        }
+    }
+
+    GlobalScope.launch(coroutineContext) {
+        for (action in actionsChannel) {
+            currentState = reducer(currentState, action)
+            output.send(currentState)
+        }
+    }
+
+    return output
 }
 
 /**
@@ -54,175 +69,14 @@ fun <S: Any, A: Any> Observable<A>.reduxStore(
  *
  * @see reduxStore
  */
-fun <S: Any, A: Any> Observable<A>.reduxStore(
+fun <S: Any, A: Any> ReceiveChannel<A>.reduxStore(
     initialState: S,
     vararg sideEffects: SideEffect<S, A>,
+    coroutineContext: CoroutineContext = Dispatchers.Unconfined,
     reducer: Reducer<S, A>
-): Observable<S> = reduxStore(
+): ReceiveChannel<S> = reduxStore(
     initialState = initialState,
     sideEffects = sideEffects.toList(),
-    reducer = reducer
+    reducer = reducer,
+    coroutineContext = coroutineContext
 )
-
-/**
- * Use [Observable.reduxStore] to create an instance of this kind of Observable.
- *
- * @param S The type of the State
- * @param A The type of the Actions
- * @see [Observable.reduxStore]
- */
-private class ObservableReduxStore<S: Any, A: Any>(
-    /**
-     * The initial state. This one will be emitted directly in onSubscribe()
-     */
-    private val initialState: S,
-    /**
-     * The upstream that emits Actions (i.e. actions triggered by an User through User Interface)
-     */
-    private val upstreamActionsObservable: Observable<A>,
-
-    /**
-     * The list of all sideEffects. A [SideEffect] takes an action, does something meaningful and returns another action.
-     * Every Action is handled by the [Reducer] to create a new State.
-     */
-    private val sideEffects: List<SideEffect<S, A>>,
-
-    /**
-     * The Reducer. Takes the current state and an action and computes a new state.
-     */
-    private val reducer: Reducer<S, A>
-) : Observable<S>(), CoroutineScope {
-
-    override val coroutineContext: CoroutineContext = Dispatchers.Unconfined
-
-    override fun subscribeActual(observer: Observer<in S>) {
-        val disposables = CompositeDisposable()
-
-        // avoids threading issues
-        val serializedObserver = SerializedObserver(observer)
-        val storeObserver = ReduxStoreObserver(
-            actualObserver = serializedObserver,
-            internalDisposables = disposables,
-            initialState = initialState,
-            reducer = reducer
-        )
-
-        // Stream to cancel the subscriptions
-        val actionsSubject = PublishSubject.create<A>()
-
-
-        actionsSubject.subscribe(storeObserver) // This will make the reducer run on each action
-
-        sideEffects.forEach { sideEffect ->
-            val actionsChannel = actionsSubject.openSubscription()
-            launch {
-                val actionOutput = sideEffect(actionsChannel, storeObserver::currentState)
-                for(action in actionOutput) {
-                    actionsSubject.onNext(action)
-                }
-            }
-        }
-
-        upstreamActionsObservable.subscribe(
-            UpstreamObserver(
-                actionsSubject = actionsSubject,
-                internalDisposables = disposables
-            )
-        )
-    }
-
-    /**
-     * Simple observer for internal reduxStore
-     */
-    private class ReduxStoreObserver<S: Any, A: Any>(
-        private val actualObserver: Observer<in S>,
-        private val internalDisposables: CompositeDisposable,
-        initialState: S,
-        private val reducer: Reducer<S, A>
-    ) : SimpleObserver<A>() {
-
-        @Volatile
-        private var state: S = initialState
-
-        /**
-         * Get the current state
-         */
-        internal fun currentState(): S = state
-
-        override fun onSubscribeActually(d: Disposable) {
-            // start with the initial state
-            actualObserver.onSubscribe(d)
-            actualObserver.onNext(currentState())
-
-            // TODO do we need to add Disposable d to internal Disposables?
-            // I think it is handled by the actualObserver.onSubscribe()
-        }
-
-        @Synchronized
-        override fun onNextActually(t: A) {
-            val currentState = currentState()
-            val newState = try {
-                reducer(currentState, t)
-            } catch (error: Throwable) {
-                onError(ReducerException(state = currentState, action = t, cause = error))
-                return
-            }
-            state = newState
-            actualObserver.onNext(newState)
-        }
-
-        override fun onErrorActually(t: Throwable) {
-            actualObserver.onError(t)
-        }
-
-        override fun onCompleteActually() {
-            actualObserver.onComplete()
-        }
-
-        override fun disposeActually() {
-            internalDisposables.dispose()
-        }
-
-        override fun isDisposedActually(): Boolean = internalDisposables.isDisposed
-    }
-
-    private operator fun CompositeDisposable.plusAssign(disposable: Disposable) {
-        this.add(disposable)
-    }
-
-    /**
-     * Observer for upstream Actions.
-     * All Actions are basically forwarded to actionSubject
-     */
-    private class UpstreamObserver<T>(
-        private val actionsSubject: Subject<T>,
-        private val internalDisposables: CompositeDisposable
-    ) : SimpleObserver<T>() {
-
-        private lateinit var disposable: Disposable
-
-        override fun onSubscribeActually(d: Disposable) {
-            disposable = d
-            internalDisposables.add(disposable)
-        }
-
-        override fun onNextActually(t: T) {
-            actionsSubject.onNext(t)
-        }
-
-        override fun onErrorActually(t: Throwable) {
-            actionsSubject.onError(t)
-        }
-
-        override fun onCompleteActually() {
-            actionsSubject.onComplete()
-        }
-
-        override fun disposeActually() {
-            // Nothing to do.
-            // InternalDisposables takes care of disposing all internal disposables at once
-        }
-
-        override fun isDisposedActually(): Boolean = disposable.isDisposed
-    }
-}
