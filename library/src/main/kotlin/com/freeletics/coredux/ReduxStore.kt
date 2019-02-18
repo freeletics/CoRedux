105009a9190f42e98d352fbad611ec907957fd5d
Yahor Berdnikau <egorr.berd@gmail.com>
2019-02-18 21:56:08 +0100

    Add subscribeToChangedState updates extension fun.
    This subscribe variant will receive only distinctUntilChanged state updates.

--- library/src/main/kotlin/com/freeletics/coredux/ReduxStore.kt
+++ library/src/main/kotlin/com/freeletics/coredux/ReduxStore.kt
@@ -187,32 +187,3 @@ class ReducerException(
  * State update will always be received on [createStore] [CoroutineScope] thread.
  */
 typealias StateReceiver<S> = (newState: S) -> Unit
-
-/**
- * Filters [StateReceiver] by only passing states that are distinct from their predecessor state using [isStateDistinct]
- * function.
- *
- * @param [isStateDistinct] receives `previousState` (can be null initially), `newState` and outputs `true` if new state
- * should be passed to [StateReceiver] or `false` if not.
- */
-fun <S : Any> StateReceiver<S>.distinctUntilChangedBy(
-    isStateDistinct: (previousState: S?, newState: S) -> Boolean
-): StateReceiver<S> {
-    var previousState: S? = null
-
-    return {
-        if (isStateDistinct(previousState, it)) {
-            previousState = it
-            this(it)
-        }
-    }
-}
-
-/**
- * Filters [StateReceiver] by only passing states that are distinct from their predecessor state using states equality,
- * only non-equal states will pass.
- */
-fun <S : Any> StateReceiver<S>.distinctUntilChanged(): StateReceiver<S> =
-    distinctUntilChangedBy { previousState, newState ->
-        previousState != newState
-    }
