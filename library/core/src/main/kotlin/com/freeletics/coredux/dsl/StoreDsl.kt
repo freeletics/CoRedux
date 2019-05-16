945a42b6714ccbc5e311129c19b014a7d5510455
Yahor Berdnikau <egorr.berd@gmail.com>
2019-05-16 22:19:22 +0200

    Set store name via separate function call.

--- library/core/src/main/kotlin/com/freeletics/coredux/dsl/StoreDsl.kt
+++ library/core/src/main/kotlin/com/freeletics/coredux/dsl/StoreDsl.kt
@@ -7,32 +7,37 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.CoroutineStart
 
 fun <S : Any, A : Any> CoroutineScope.describeStore(
-    storeName: String,
     builder: StoreBuilder<S, A>.() -> Unit
 ): Store<S, A> {
-    val storeBuilder = StoreBuilder<S, A>(storeName, this)
+    val storeBuilder = StoreBuilder<S, A>(this)
     storeBuilder.builder()
     return storeBuilder.createStore()
 }
 
 class StoreBuilder<S : Any, A : Any>(
-    private val storeName: String,
     private val coroutineScope: CoroutineScope
 ) {
     private var logSinks: List<LogSink> = emptyList()
     private var startMode: CoroutineStart = CoroutineStart.LAZY
+    private var storeName: String? = null
     private val reducerBuilder = ReducerBuilder<S, A>()
     private val sideEffectsBuilder = SideEffectsBuilder<S, A>()
 
     internal fun createStore(): Store<S, A> {
+        val name = requireNotNull(storeName)
+
         return coroutineScope.createStore(
-            name = storeName,
+            name = name,
             initialState = reducerBuilder.initialState,
             sideEffects = sideEffectsBuilder.build(),
             launchMode = startMode,
             logSinks = logSinks,
             reducer = reducerBuilder.build()
         )
+    }
+
+    fun name(storeName: String) {
+        this.storeName = storeName
     }
 
     fun enableLogging(logSinks: List<LogSink>) {
